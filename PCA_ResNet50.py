69cbe313f1c13c286d1a8eb8984978f71f98b3e0
liuyingbin19222 <212164693@qq.com>
2020-02-17 11:06:30 +0800

    pca

---
+++ PCA_ResNet50.py
@@ -0,0 +1,351 @@
+import keras
+from keras.layers import Conv2D, Conv3D, Flatten, Dense, Reshape, BatchNormalization
+from keras.layers import Dropout, Input
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report, cohen_kappa_score
+
+from operator import truediv
+
+from plotly.offline import init_notebook_mode
+
+import numpy as np
+import tensorflow as tf
+
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from keras.initializers import glorot_uniform
+
+import pydot
+from IPython.display import SVG
+import scipy.misc
+from matplotlib.pyplot import imshow
+import keras.backend as K
+K.set_image_data_format('channels_last')
+K.set_learning_phase(1)
+
+
+from keras.utils import to_categorical
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.io as sio
+import os
+import spectral
+
+## GLOBAL VARIABLES
+dataset = 'IP'
+test_ratio = 0.8
+windowSize = 25
+
+def loadData(name):
+    data_path = os.path.join(os.getcwd(),'data')
+    if name == 'IP':
+        data = sio.loadmat(os.path.join(data_path, 'Indian_pines_corrected.mat'))['indian_pines_corrected']
+        labels = sio.loadmat(os.path.join(data_path, 'Indian_pines_gt.mat'))['indian_pines_gt']
+    elif name == 'SA':
+        data = sio.loadmat(os.path.join(data_path, 'Salinas_corrected.mat'))['salinas_corrected']
+        labels = sio.loadmat(os.path.join(data_path, 'Salinas_gt.mat'))['salinas_gt']
+    elif name == 'PU':
+        data = sio.loadmat(os.path.join(data_path, 'PaviaU.mat'))['paviaU']
+        labels = sio.loadmat(os.path.join(data_path, 'PaviaU_gt.mat'))['paviaU_gt']
+    
+    return data, labels
+
+def splitTrainTestSet(X, y, testRatio, randomState=345):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=testRatio, random_state=randomState,
+                                                        stratify=y)
+    return X_train, X_test, y_train, y_test
+
+
+def applyPCA(X, numComponents=75):
+    newX = np.reshape(X, (-1, X.shape[2]))
+    pca = PCA(n_components=numComponents, whiten=True)
+    newX = pca.fit_transform(newX)
+    newX = np.reshape(newX, (X.shape[0],X.shape[1], numComponents))
+    return newX, pca
+
+def padWithZeros(X, margin=2):
+    newX = np.zeros((X.shape[0] + 2 * margin, X.shape[1] + 2* margin, X.shape[2]))
+    x_offset = margin
+    y_offset = margin
+    newX[x_offset:X.shape[0] + x_offset, y_offset:X.shape[1] + y_offset, :] = X
+    return newX
+
+# 去零
+def createImageCubes(X, y, windowSize=5, removeZeroLabels = True):
+    margin = int((windowSize - 1) / 2)
+    zeroPaddedX = padWithZeros(X, margin=margin)
+    # split patches
+    patchesData = np.zeros((X.shape[0] * X.shape[1], windowSize, windowSize, X.shape[2]))
+    patchesLabels = np.zeros((X.shape[0] * X.shape[1]))
+    patchIndex = 0
+    for r in range(margin, zeroPaddedX.shape[0] - margin):
+        for c in range(margin, zeroPaddedX.shape[1] - margin):
+            patch = zeroPaddedX[r - margin:r + margin + 1, c - margin:c + margin + 1]   
+            patchesData[patchIndex, :, :, :] = patch
+            patchesLabels[patchIndex] = y[r-margin, c-margin]
+            patchIndex = patchIndex + 1
+    if removeZeroLabels:
+        patchesData = patchesData[patchesLabels>0,:,:,:]
+        patchesLabels = patchesLabels[patchesLabels>0]
+        patchesLabels -= 1
+    return patchesData, patchesLabels
+
+X, y = loadData(dataset)
+K = 30 if dataset == 'IP' else 15
+X,pca = applyPCA(X,numComponents=K)
+X, y = createImageCubes(X, y, windowSize=windowSize)
+
+##  
+Xtrain, Xtest, ytrain, ytest = splitTrainTestSet(X, y, test_ratio)
+
+# print("Xtrain.shape:",Xtrain.shape)
+# print("ytrain.shape:",ytrain.shape)
+# print("ytrain:",ytrain)
+def convert_one_hot(labels,classes=16):
+    return to_categorical(labels,num_classes=classes)
+ytrain = convert_one_hot(ytrain,16)
+ytest = convert_one_hot(ytest,16)
+# print("ytrain.shape:",ytrain.shape)
+# ResNet50 网络;
+def identity_block(X, f, filters, stage, block):
+    """
+    实现图3的恒等块
+    
+    参数：
+        X - 输入的tensor类型的数据，维度为( m, n_H_prev, n_W_prev, n_H_prev )
+        f - 整数，指定主路径中间的CONV窗口的维度
+        filters - 整数列表，定义了主路径每层的卷积层的过滤器数量
+        stage - 整数，根据每层的位置来命名每一层，与block参数一起使用。
+        block - 字符串，据每层的位置来命名每一层，与stage参数一起使用。
+        
+    返回：
+        X - 恒等块的输出，tensor类型，维度为(n_H, n_W, n_C)
+    
+    """
+    
+    #定义命名规则
+    conv_name_base = "res" + str(stage) + block + "_branch"
+    bn_name_base   = "bn"  + str(stage) + block + "_branch"
+    
+    #获取过滤器
+    F1, F2, F3 = filters
+    
+    #保存输入数据，将会用于为主路径添加捷径
+    X_shortcut = X
+    
+    #主路径的第一部分
+    ##卷积层
+    X = Conv2D(filters=F1, kernel_size=(1,1), strides=(1,1) ,padding="valid",
+               name=conv_name_base+"2a", kernel_initializer=glorot_uniform(seed=0))(X)
+    ##归一化
+    X = BatchNormalization(axis=3,name=bn_name_base+"2a")(X)
+    ##使用ReLU激活函数
+    X = Activation("relu")(X)
+    
+    #主路径的第二部分
+    ##卷积层
+    X = Conv2D(filters=F2, kernel_size=(f,f),strides=(1,1), padding="same",
+               name=conv_name_base+"2b", kernel_initializer=glorot_uniform(seed=0))(X)
+    ##归一化
+    X = BatchNormalization(axis=3,name=bn_name_base+"2b")(X)
+    ##使用ReLU激活函数
+    X = Activation("relu")(X)
+    
+    
+    #主路径的第三部分
+    ##卷积层
+    X = Conv2D(filters=F3, kernel_size=(1,1), strides=(1,1), padding="valid",
+               name=conv_name_base+"2c", kernel_initializer=glorot_uniform(seed=0))(X)
+    ##归一化
+    X = BatchNormalization(axis=3,name=bn_name_base+"2c")(X)
+    ##没有ReLU激活函数
+    
+    #最后一步：
+    ##将捷径与输入加在一起
+    X = Add()([X,X_shortcut])
+    ##使用ReLU激活函数
+    X = Activation("relu")(X)
+    
+    return X
+
+
+def convolutional_block(X, f, filters, stage, block, s=2):
+    """
+    实现图5的卷积块
+    
+    参数：
+        X - 输入的tensor类型的变量，维度为( m, n_H_prev, n_W_prev, n_C_prev)
+        f - 整数，指定主路径中间的CONV窗口的维度
+        filters - 整数列表，定义了主路径每层的卷积层的过滤器数量
+        stage - 整数，根据每层的位置来命名每一层，与block参数一起使用。
+        block - 字符串，据每层的位置来命名每一层，与stage参数一起使用。
+        s - 整数，指定要使用的步幅
+    
+    返回：
+        X - 卷积块的输出，tensor类型，维度为(n_H, n_W, n_C)
+    """
+    
+    #定义命名规则
+    conv_name_base = "res" + str(stage) + block + "_branch"
+    bn_name_base   = "bn"  + str(stage) + block + "_branch"
+    
+    #获取过滤器数量
+    F1, F2, F3 = filters
+    
+    #保存输入数据
+    X_shortcut = X
+    
+    #主路径
+    ##主路径第一部分
+    X = Conv2D(filters=F1, kernel_size=(1,1), strides=(s,s), padding="valid",
+               name=conv_name_base+"2a", kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3,name=bn_name_base+"2a")(X)
+    X = Activation("relu")(X)
+    
+    ##主路径第二部分
+    X = Conv2D(filters=F2, kernel_size=(f,f), strides=(1,1), padding="same",
+               name=conv_name_base+"2b", kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3,name=bn_name_base+"2b")(X)
+    X = Activation("relu")(X)
+    
+    ##主路径第三部分
+    X = Conv2D(filters=F3, kernel_size=(1,1), strides=(1,1), padding="valid",
+               name=conv_name_base+"2c", kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3,name=bn_name_base+"2c")(X)
+    
+    #捷径
+    X_shortcut = Conv2D(filters=F3, kernel_size=(1,1), strides=(s,s), padding="valid",
+               name=conv_name_base+"1", kernel_initializer=glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3,name=bn_name_base+"1")(X_shortcut)
+    
+    #最后一步
+    X = Add()([X,X_shortcut])
+    X = Activation("relu")(X)
+    
+    return X
+
+
+def ResNet50(input_shape=(25,25,30),classes=16):
+    """
+    实现ResNet50
+    CONV2D -> BATCHNORM -> RELU -> MAXPOOL -> CONVBLOCK -> IDBLOCK*2 -> CONVBLOCK -> IDBLOCK*3
+    -> CONVBLOCK -> IDBLOCK*5 -> CONVBLOCK -> IDBLOCK*2 -> AVGPOOL -> TOPLAYER
+    
+    参数：
+        input_shape - 图像数据集的维度
+        classes - 整数，分类数
+        
+    返回：
+        model - Keras框架的模型
+        
+    """
+    
+    #定义tensor类型的输入数据
+    X_input = Input(input_shape)
+    
+    #0填充
+    X = ZeroPadding2D((3,3))(X_input)
+    
+    #stage1
+    X = Conv2D(filters=64, kernel_size=(7,7), strides=(2,2), name="conv1",
+               kernel_initializer=glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis=3, name="bn_conv1")(X)
+    X = Activation("relu")(X)
+    X = MaxPooling2D(pool_size=(3,3), strides=(2,2))(X)
+    
+    #stage2
+    X = convolutional_block(X, f=3, filters=[64,64,256], stage=2, block="a", s=1)
+    X = identity_block(X, f=3, filters=[64,64,256], stage=2, block="b")
+    X = identity_block(X, f=3, filters=[64,64,256], stage=2, block="c")
+    
+    #stage3
+    X = convolutional_block(X, f=3, filters=[128,128,512], stage=3, block="a", s=2)
+    X = identity_block(X, f=3, filters=[128,128,512], stage=3, block="b")
+    X = identity_block(X, f=3, filters=[128,128,512], stage=3, block="c")
+    X = identity_block(X, f=3, filters=[128,128,512], stage=3, block="d")
+    
+    #stage4
+    X = convolutional_block(X, f=3, filters=[256,256,1024], stage=4, block="a", s=2)
+    X = identity_block(X, f=3, filters=[256,256,1024], stage=4, block="b")
+    X = identity_block(X, f=3, filters=[256,256,1024], stage=4, block="c")
+    X = identity_block(X, f=3, filters=[256,256,1024], stage=4, block="d")
+    X = identity_block(X, f=3, filters=[256,256,1024], stage=4, block="e")
+    X = identity_block(X, f=3, filters=[256,256,1024], stage=4, block="f")
+    
+    #stage5
+    X = convolutional_block(X, f=3, filters=[512,512,2048], stage=5, block="a", s=2)
+    X = identity_block(X, f=3, filters=[512,512,2048], stage=5, block="b")
+    X = identity_block(X, f=3, filters=[512,512,2048], stage=5, block="c")
+    
+    #均值池化层
+    X = AveragePooling2D(pool_size=(2,2),padding="same")(X)
+    
+    #输出层
+    X = Flatten()(X)
+    X = Dense(classes, activation="softmax", name="fc"+str(classes),
+              kernel_initializer=glorot_uniform(seed=0))(X)
+    
+    
+    #创建模型
+    model = Model(inputs=X_input, outputs=X, name="ResNet50")
+    
+    return model
+
+# # x_train : (3074,25,25,30)   y_train: (3074)
+# model = ResNet50(input_shape=(25,25,30),classes=16)
+# model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+#
+#
+# model.fit(Xtrain,ytrain,epochs=2,batch_size=25)
+# preds = model.evaluate(Xtest,ytest)
+#
+# print("误差率:",str(preds[0]))
+# print("准确率:",str(preds[1]))
+
+def main():
+    # x_train : (3074,25,25,30)   y_train: (3074)
+    model = ResNet50(input_shape=(25, 25, 30), classes=16)
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+    history = model.fit(Xtrain, ytrain, epochs=100, batch_size=25)
+    preds = model.evaluate(Xtest, ytest)
+    plt.figure(figsize=(7,7))
+    plt.grid()
+    plt.plot(history.history['loss'])
+    #plt.plot(history.history['val_loss'])
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend(['Training','Validation'], loc='upper right')
+    plt.savefig("loss_curve.txt")
+    plt.show()
+
+    plt.figure(figsize=(5,5))
+    plt.ylim(0,1.1)
+    plt.grid()
+    plt.plot(history.history['acc'])
+    #plt.plot(history.history['val_acc'])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend(['Training','Validation'])
+    plt.savefig("acc_curve.txt")
+    plt.show()
+
+
+    print("误差率:", str(preds[0]))
+    print("准确率:", str(preds[1]))
+
+if __name__ == "__main__":
+    main()
